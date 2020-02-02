43c1f18a08b426807962a8e1d4c9868419939fb4
kmbrkat <davidm.cambre@gmail.com>
2020-02-02 20:16:25 +0100

    to execute the game under C64 Emulator

---
+++ run64.bat
@@ -0,0 +1,11 @@
+
+@echo Compile the tape and dsk files 
+bas2tap loader.bas -a10 -sjuego
+REM bin2tap .\res\zhl.scr -o screen.tap
+REM copy /b loader.tap+screen.tap+parser.tap+p1.tap+p4.tap+p6.tap+p0.tap juego.tap
+REM copy /b loader.tap+screen.tap+parser.tap juego.tap
+copy /b loader.tap+parser.tap juego.tap
+..\spectrum\Spectaculator.exe juego.tap
+REM cd ..\ZEsarUX_win-beta-8.1
+REM .\zesarux.exe ..\La-Legion-espacial\juego.tap
+REM cd ..\La-Legion-espacial
