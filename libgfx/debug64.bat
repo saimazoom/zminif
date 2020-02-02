857f3528ab54b3071e5ee5fe019af8a2682235bc
kmbrkat <davidm.cambre@gmail.com>
2020-02-02 20:17:00 +0100

    Execute the code via the Debugger

---
+++ libgfx/debug64.bat
@@ -0,0 +1,13 @@
+
+@echo Compile the tape and dsk files 
+REM bas2tap loader.bas -a10 -sjuego
+REM bin2tap .\res\zhl.scr -o screen.tap
+REM copy /b loader.tap+screen.tap+parser.tap+p1.tap+p4.tap+p6.tap+p0.tap juego.tap
+REM copy /b loader.tap+screen.tap+parser.tap juego.tap
+REM copy /b loader.tap+parser.tap juego.tap
+REM ..\spectrum\Spectaculator.exe juego.tap
+REM cd ..\ZEsarUX_win-beta-8.1
+REM .\zesarux.exe ..\La-Legion-espacial\juego.tap
+REM cd ..\La-Legion-espacial
+
+D:\devel\C64-65XE-Debugger-v0.64.56.8-win32\C64-65XE-Debugger-v0.64.56.8\C64Debugger.exe D:\devel\zminif\libgfx\libgfx.prg
